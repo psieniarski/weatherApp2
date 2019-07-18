5de2be777bcb91cbab58697581d1369490718f0f
Pawel Sieniarski <paabson@gmail.com>
2019-07-18 18:11:46 +0200

    add weather reducer tests

---
+++ src/reducers/__tests__/weather.js
@@ -0,0 +1,28 @@
+import { weather, initialState } from '../weather';
+import { FETCH_WEATHER_SUCCESS } from '../../actions/actionTypes';
+
+describe('weather reducer', () => {
+  it('should return the initial state', () => {
+    expect(weather(undefined, {})).toEqual(initialState);
+  });
+  it('should handle success fetch response', () => {
+    expect(
+      weather([], {
+        type: FETCH_WEATHER_SUCCESS,
+        payload: {
+          weather: [{ icon: 'test' }],
+          main: {
+            temp: 0,
+            temp_min: 1,
+            temp_max: 2
+          }
+        }
+      })
+    ).toEqual({
+      icon: 'test',
+      temperature: 0,
+      temperatureMin: 1,
+      temperatureMax: 2
+    });
+  });
+});
