4a0f225481769c4a9e7da46cb4b854a6870c8f31
Pawel Sieniarski <paabson@gmail.com>
2019-07-18 19:22:17 +0200

    add weather icons

--- src/components/core/Icon.jsx
+++ src/components/core/Icon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Icon = ({ name }) => {
-  return <i className={`icon ${name}`}></i>;
+  return <i className={`wi ${name} icon-xl`}></i>;
 };
 
 Icon.propTypes = {
