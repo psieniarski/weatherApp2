0dc8ca190854db33de20acbb7b09ad650057bde6
Pawel Sieniarski <paabson@gmail.com>
2019-07-19 17:25:47 +0200

    add test

---
+++ src/actions/__tests__/index.js
@@ -0,0 +1,11 @@
+import { fetchWeatherData, APP_ID } from '../index';
+import { RSAA } from 'redux-api-middleware';
+
+describe('fetchWeatherData', () => {
+  it('should parse proper endpoint', () => {
+    const result = fetchWeatherData({ q: 'test' });
+    expect(result[RSAA].endpoint).toBe(
+      `http://api.openweathermap.org/data/2.5/weather?appid=${APP_ID}&units=imperial&q=test`
+    );
+  });
+});
--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,14 @@ import {
   FETCH_WEATHER_SUCCESS,
   FETCH_WEATHER_FAILURE
 } from './actionTypes';
+
+export const APP_ID = process.env.REACT_APP_OPEN_WEATHER_MAP_APPID || '';
+
 export function fetchWeatherData({ q = '', temperatureUnit } = {}) {
   const units = temperatureUnit === 'celcius' ? 'metric' : 'imperial';
-  const appid = process.env.REACT_APP_OPEN_WEATHER_MAP_APPID;
   return {
     [RSAA]: {
-      endpoint: `http://api.openweathermap.org/data/2.5/weather?appid=${appid}&units=${units}&q=${q}`,
+      endpoint: `http://api.openweathermap.org/data/2.5/weather?appid=${APP_ID}&units=${units}&q=${q}`,
       method: 'GET',
       types: [
         FETCH_WEATHER_REQUEST,
