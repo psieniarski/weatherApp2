bdf1aa8d1daf7a7e0ad97c737dd60357d99f93a7
Pawel Sieniarski <paabson@gmail.com>
2019-07-24 21:31:26 +0200

    update tests

--- src/actions/__tests__/index.js
+++ src/actions/__tests__/index.js
@@ -5,7 +5,7 @@ describe('fetchWeatherData', () => {
   it('should parse proper endpoint', () => {
     const result = fetchWeatherData({ q: 'test' });
     expect(result[RSAA].endpoint).toBe(
-      `http://api.openweathermap.org/data/2.5/weather?appid=${APP_ID}&units=imperial&q=test`
+      `http://api.openweathermap.org/data/2.5/weather?appid=${APP_ID}&units=metric&q=test`
     );
   });
 });
