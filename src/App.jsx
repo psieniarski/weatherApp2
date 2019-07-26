b1bab53bd2710bd7d4818bfe36d0c459e803481f
Pawel Sieniarski <paabson@gmail.com>
2019-07-26 09:48:10 +0200

    mv NotFoundPage to routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './styles/styles.scss';
 import { useRoutes } from 'hookrouter';
 import routes from './routes';
-import NotFoundPage from './views/NotFoundPage';
 import Navbar from './containers/Navbar';
 
 function App() {
@@ -10,7 +9,7 @@ function App() {
   return (
     <React.Fragment>
       <Navbar />
-      {routeResult || <NotFoundPage />}
+      {routeResult}
     </React.Fragment>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from '../views/Home';
 import City from '../views/City';
+import NotFoundPage from '../views/NotFoundPage';
 
 const routes = {
   '/': () => <Home />,
-  '/cities/:name': ({ name }) => <City name={name} />
+  '/cities/:name': ({ name }) => <City name={name} />,
+  '*': () => <NotFoundPage />
 };
 
 export default routes;
